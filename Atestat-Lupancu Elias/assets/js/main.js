8ad2d282980b6d6ea35639db4e4e29004b60168d
EliasLpc <162722144+EliasLpc@users.noreply.github.com>
2025-05-06 01:28:52 +0300

    Add files via upload

---
+++ Atestat-Lupancu Elias/assets/js/main.js
@@ -0,0 +1,64 @@
+
+
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+   
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== ADD BLUR HEADER ===============*/
+
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+   
+    this.scrollY >= 50 ? header.classList.add('blur-header') 
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
+
+
+const swiperTravel = new Swiper('.travel__swiper', {
+    loop: true,
+    spaceBetween: '32',
+    grabCursor: true,
+    slidesPerView: 'auto',
+    centeredSlides: 'auto',
+    
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    breakpoints:{
+        600:{
+            slidesPerView: 2,
+        },
+        900:{
+            slidesPerView: 3,
+        }
+    }
+  })
+
+ 
+
